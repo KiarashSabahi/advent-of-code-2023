f01621aa1f58489c018c81df5165c9d140521104
ksaba <ksabahi2001@gmail.com>
2023-12-02 18:09:57 +0330

    feat: refactor part one and add part 2

--- src/solutions/day2/mod.rs
+++ src/solutions/day2/mod.rs
@@ -9,20 +9,13 @@ struct Game {
 }
 
 pub fn cube_conundrum() {
-    let mut games: Vec<Game> = Vec::new();
-    parser(&mut games);
-
-    let mut sum = 0;
-    for game in games {
-        println!("{:?}", game);
-        if game.blue <= 14 && game.green <= 13 && game.red <= 12 {
-            sum += game.id;
-        }
-    }
-    println!("{}", sum);
+    parser();
 }
 
-fn parser(games: &mut Vec<Game>) {
+fn parser() {
+    let mut sum_part1 = 0;
+    let mut sum_part2 = 0;
+
     let contents = fs::read_to_string("./src/solutions/day2/input.txt")
         .expect("Should have been able to read the file");
     // let contents = "Game 100: 2 green, 1 blue; 9 red, 8 green, 1 blue; 4 red, 10 green, 1 blue; 17 green, 8 red; 5 green, 1 blue, 7 red; 14 red, 12 green";
@@ -38,11 +31,19 @@ fn parser(games: &mut Vec<Game>) {
 
         let id = &line[5..index_pointer];
         index_pointer +=2;
-        games.push(process_cubes(&line[index_pointer..], index as i32));
+        let bool_part1 = process_cubes_part1(&line[index_pointer..]);
+        if bool_part1 == true {
+            sum_part1 += index + 1;;
+        }
+
+        sum_part2 += process_cubes_part2(&line[index_pointer..]);;
+
     }
+    println!("{}", sum_part1);
+    println!("{}", sum_part2);
 }
 
-fn process_cubes(line: &str, index: i32) -> Game {
+fn process_cubes_part1(line: &str) -> bool {
 
 
     // println!("{line}");
@@ -85,12 +86,59 @@ fn process_cubes(line: &str, index: i32) -> Game {
         // println!("red: {red_count}");
 
         if index_pointer >= line.len() {
-            return Game {
-                id: index + 1,
-                blue: blue_count,
-                red: red_count,
-                green: green_count,
+            if blue_count <= 14 && green_count <= 13 && red_count <= 12 {
+                return true;
+            } else { return false; }
+        }
+
+        index_pointer += 2;
+    }
+}
+
+fn process_cubes_part2(line: &str) -> i32 {
+
+
+    // println!("{line}");
+    let mut index_pointer = 0;
+    let mut length_pointer = 1;
+
+    let mut green_count= 0;
+    let mut red_count= 0;
+    let mut blue_count= 0;
+
+
+    loop {
+        let new = &line[index_pointer..];
+        length_pointer = line[index_pointer..].find(' ').unwrap();
+        let count = line[index_pointer..index_pointer + length_pointer].parse::<i32>().unwrap();
+        index_pointer += length_pointer;
+        let color_letter = &line[index_pointer + 1..2 + index_pointer];
+
+        if color_letter == "g" {
+            index_pointer += 6;
+            if count > green_count {
+                green_count = count;
             }
+        } else if color_letter == "r" {
+            index_pointer += 4;
+            if count > red_count {
+                red_count = count;
+            }
+        } else if color_letter == "b" {
+            index_pointer += 5;
+            if count > blue_count {
+                blue_count = count;
+            }
+        } else {
+            panic!("error parsing")
+        }
+
+        // println!("green: {green_count}");
+        // println!("blue: {blue_count}");
+        // println!("red: {red_count}");
+
+        if index_pointer >= line.len() {
+            return blue_count * red_count * green_count;
         }
 
         index_pointer += 2;
