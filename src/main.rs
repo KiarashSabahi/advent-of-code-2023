b128bca1479f8cbb2b6473e7c5669c24c8359c6d
ksaba <ksabahi2001@gmail.com>
2023-12-03 17:02:03 +0330

    feat: mic

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::time::{Duration, Instant};
 
 fn main() {
     let mut time = Duration::new(0, 0);
-    let count  = 50;
+    let count  = 1;
     for _ in 0..count {
         println!("Start");
         let now = Instant::now();
-        solutions::day1::trebuchet();
+        solutions::day3::gear_ratios();
 
         let elapsed = now.elapsed();
         time += elapsed;
