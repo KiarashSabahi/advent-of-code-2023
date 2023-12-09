e99a6770443ee0a5f63deb21b6b356e63c6a964e
ksaba <ksabahi2001@gmail.com>
2023-12-09 15:06:37 +0330

    feat: add day 9 part 2

--- src/solutions/day9/mod.rs
+++ src/solutions/day9/mod.rs
@@ -5,6 +5,7 @@ pub fn mirage_maintenance() {
         .expect("Should have been able to read the file");
 
     println!("{}", part1(&contents));
+    println!("{}", part2(&contents));
 }
 
 fn part1(input: &String) -> i32 {
@@ -17,6 +18,15 @@ fn part1(input: &String) -> i32 {
     sum
 }
 
+fn part2(input: &String) -> i32 {
+    let mut sum = 0;
+    for line in input.lines() {
+        let nums:Vec<i32> = line.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect();
+        sum += recursive_iteration_part2(nums);
+    }
+
+    sum
+}
 
 fn recursive_iteration_part1(line: Vec<i32>) -> i32 {
     let length = line.len();
@@ -31,6 +41,20 @@ fn recursive_iteration_part1(line: Vec<i32>) -> i32 {
     }
 }
 
+fn recursive_iteration_part2(line: Vec<i32>) -> i32 {
+    let length = line.len();
+    if line[length - 1] == 0 && line[0] == 0 {
+        0
+    } else {
+        let mut next_line: Vec<i32> = Vec::new();
+        for i in 0..length-1 {
+            next_line.push(line[i + 1] - line[i])
+        }
+        line[0] - recursive_iteration_part2(next_line)
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,5 +65,14 @@ mod tests {
 1 3 6 10 15 21
 10 13 16 21 30 45");
         assert_eq!(part1(&input), 114 );
+    }
+
+    #[test]
+    fn test2() {
+        let input = String::from(
+            "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45");
+        assert_eq!(part2(&input), 2 );
     }
 }
