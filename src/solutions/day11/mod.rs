a506672dfc1d4969baf119ef95e59aa7e442c517
ksaba <ksabahi2001@gmail.com>
2023-12-12 16:56:05 +0330

    feat: add day 11 misc

--- src/solutions/day11/mod.rs
+++ src/solutions/day11/mod.rs
@@ -1,3 +1,48 @@
+use std::fs;
+
 pub fn cosmic_expansion() {
+    let contents = fs::read_to_string("./src/solutions/day9/input.txt")
+        .expect("Should have been able to read the file");
+
+    let contents = String::from("...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....");
+
+    println!("{}", part1(&contents));
+}
+
+fn part1(input: &String) -> usize{
+
+
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        let input = String::from(
+            "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....");
+        assert_eq!(part1(&input), 374);
+    }
+
 
 }
