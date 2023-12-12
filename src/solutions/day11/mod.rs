4637ab7dfd82c86f86bf0110c672f1b708ed0516
Kiarash <kiarash.sabahi@snapp.cab>
2023-12-12 18:43:38 +0330

    feat: calculate not empty rows and collums

--- src/solutions/day11/mod.rs
+++ src/solutions/day11/mod.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, vec};
 
 pub fn cosmic_expansion() {
     let contents = fs::read_to_string("./src/solutions/day9/input.txt")
@@ -21,8 +21,31 @@ pub fn cosmic_expansion() {
 fn part1(input: &String) -> usize{
 
 
+    let mut galaxies: Vec<(usize, usize)> = Vec::new();
+    for (i, line) in input.lines().enumerate() {
+        for (j, char) in line.chars().enumerate() {
+            if char == '#' {
+                galaxies.push((i, j));
+            }
+        }
+    }
+    println!("{:?}", galaxies);
+
+    for start in &galaxies {
+       for end in &galaxies {
+            if end == start {
+                continue;
+            }
+            println!("{:?}{:?}{:?}", start, end, start.0.abs_diff(end.0) + start.1.abs_diff(end.1));
+       } 
+    }
+
 
     0
+}
+
+fn is_row_empty(galaxies: &Vec<(usize, usize)>) -> bool {
+    false
 }
 
 #[cfg(test)]
