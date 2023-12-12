d1ce36fbd43120cdcf114b658b43bba69a85a9eb
ksaba <ksabahi2001@gmail.com>
2023-12-12 16:44:20 +0330

    feat: add cache for day 12

--- src/solutions/day12/mod.rs
+++ src/solutions/day12/mod.rs
@@ -1,10 +1,11 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn hot_springs() {
     let contents = fs::read_to_string("./src/solutions/day12/input.txt")
         .expect("Should have been able to read the file");
-    let contents = String::from(
-        ".??..??...?##. 1,1,3");
+    // let contents = String::from(
+    //     ".??..??...?##. 1,1,3");
     println!("{}", part1(&contents));
     println!("{}", part2(&contents));
 
@@ -16,7 +17,8 @@ fn part1(contents: &String) -> usize {
         let mut split = line.split_whitespace();
         let pattern = split.next().unwrap().as_bytes();
         let numbers = split.next().unwrap().split(',').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>();
-        let x = possible_ways(&pattern, &numbers, 0);
+        let mut cache = HashMap::new();
+        let x = possible_ways(&mut cache, &pattern, &numbers, 0);
         sum += x;
     }
     sum
@@ -35,13 +37,14 @@ fn part2(contents: &String) -> usize {
         }
         let new_nums = (0..5).flat_map(|_| &numbers).copied().collect::<Vec<_>>();
         println!("{} {:?}", new_pattern, new_nums);
-        let x = possible_ways(&new_pattern.as_bytes(), &new_nums, 0);
+        let mut cache = HashMap::new();
+        let x = possible_ways(&mut cache, &new_pattern.as_bytes(), &new_nums, 0);
         sum += x;
     }
     sum
 }
 
-fn possible_ways(pattern: &[u8], numbers: &[usize], len: usize) -> usize {
+fn possible_ways(cache: &mut HashMap<(usize, usize, usize), usize>,pattern: &[u8], numbers: &[usize], len: usize) -> usize {
     // println!("{:?}", String::from_utf8(Vec::from(pattern)).unwrap().chars().collect::<Vec<_>>()[0]);
     // println!("{:?} {:?} {:?}", String::from_utf8(Vec::from(pattern)).unwrap(), numbers, len);
 
@@ -59,39 +62,43 @@ fn possible_ways(pattern: &[u8], numbers: &[usize], len: usize) -> usize {
         return 0;
     }
 
+    let key = (pattern.len(), len, numbers.len());
+    if let Some(&x) = cache.get(&key) {
+        return x;
+    }
+
     let mut ways: usize = 0;
     match pattern[0] {
         b'.' => {
             if len == 0 {
-                ways = possible_ways(&pattern[1..], numbers, 0);
+                ways = possible_ways(cache, &pattern[1..], numbers, 0);
             } else if len != numbers[0] {
                 ways = 0;
             } else {
-                ways = possible_ways(&pattern[1..], &numbers[1..], 0);
+                ways = possible_ways(cache, &pattern[1..], &numbers[1..], 0);
             }
         },
         b'#' => {
             if len == 0 {
-                ways = possible_ways(&pattern[1..], numbers, 1);
+                ways = possible_ways(cache, &pattern[1..], numbers, 1);
             } else {
-                ways = possible_ways(&pattern[1..], numbers, len + 1);
+                ways = possible_ways(cache, &pattern[1..], numbers, len + 1);
             }
         },
         b'?' => {
             if len == 0 {
-                ways += possible_ways(&pattern[1..], numbers, 0);
-                ways += possible_ways(&pattern[1..], numbers, 1);
+                ways += possible_ways(cache, &pattern[1..], numbers, 0);
+                ways += possible_ways(cache, &pattern[1..], numbers, 1);
             } else {
-                let mut ans =  possible_ways(&pattern[1..], numbers, len + 1);
+                let mut ans =  possible_ways(cache, &pattern[1..], numbers, len + 1);
                 if len == numbers[0] {
-                    ans += possible_ways(&pattern[1..], &numbers[1..], 0);
+                    ans += possible_ways(cache, &pattern[1..], &numbers[1..], 0);
                 }
                 ways += ans;
-                return ans;
             }
         },
         _ => ways = 0
     };
-
+    cache.insert(key, ways);
     ways
 }
