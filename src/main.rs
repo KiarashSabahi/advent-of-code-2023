726e9c7e591a35f720a2ce7e9f18da248482a13c
ksaba <ksabahi2001@gmail.com>
2023-12-16 04:52:12 +0330

    chore: day 14 misc

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,10 @@ fn main() {
     for _ in 0..count {
         println!("Start");
         let now = Instant::now();
-        solutions::day13::point_of_incidence();
+        solutions::day14::parabolic_reflector_dish();
 
         let elapsed = now.elapsed();
         time += elapsed;
-
     }
 
     println!("Elapsed: {:.2?}", time/count);
