769bb0741001feded7c9e227d35da7a5be47572c
ksaba <ksabahi2001@gmail.com>
2023-12-16 04:52:33 +0330

    feat: add day 14 part 1 solution

---
+++ src/solutions/day14/mod.rs
@@ -0,0 +1,63 @@
+use std::fs;
+
+pub fn parabolic_reflector_dish() {
+    let contents = fs::read_to_string("./src/solutions/day14/input.txt")
+        .expect("Should have been able to read the file");
+
+//     let contents = String::from("O....#....
+// O.OO#....#
+// .....##...
+// OO.#O....O
+// .O.....O#.
+// O.#..O.#.#
+// ..O..#O..O
+// .......O..
+// #....###..
+// #OO..#....");
+
+    println!("\npart1: {:?}", part1(&contents));
+}
+
+fn part1(contents: &String) -> usize {
+    println!("{}\n", contents);
+
+    let mut dish:Vec<Vec<char>> = Vec::new();
+    for line in contents.lines() {
+        if !line.is_empty() {
+            dish.push(line.chars().collect::<Vec<char>>())
+        }
+    }
+
+    let mut sum = 0;
+
+    for i in 0..dish.len() {
+        for j in 0..dish[i].len() {
+            if dish[i][j] != 'O' { continue }
+            let mut target = i;
+            while target != 0{
+                if dish[target - 1][j] != '.'{
+                    break;
+                } else {
+                    target -= 1;
+                }
+            }
+
+            if target != i {
+                dish[i][j] = '.';
+                dish[target][j] = 'O';
+            }
+
+        }
+    }
+
+
+    for i in 0..dish.len() {
+        for j in 0..dish[i].len() {
+            if dish[i][j] == 'O' {
+                sum += dish.len() - i;
+                println!("{}", dish.len() - i);
+            }
+        }
+    }
+    sum
+}
